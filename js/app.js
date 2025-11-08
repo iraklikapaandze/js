df608793c4867616aade7478c7db9406343520a2
iraklikapaandze <kapanadzeirakli0201@gmail.com>
2025-11-08 11:35:02 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// 13 =>number type
+// true/false => boolian type
+// { name:irakli}- => object type
+// { age:13} => object type
+// "favoriteColor:red" => string type
+// "SchoolsName:141" => string type
+// "country:georgia" => string type
+// "FavoriteSong:eye of the tiger" => string type
+// "favorite movie:truman show" => string type
+// "FunFact:i like playing videogames" => string type
+// [videogames, coding, football] => array type
+
+
+
+let name="irakli"
+let age="13"
+let favoriteColor="red"
+let SchoolsName="141"
+let country="georgia"
+let hobbies=["videogames", "coding", "football"]
+let FavoriteSong="eye of the tiger"
+let FavoriteMovie="truman show"
+let FunFact="i like playign video games"
+let likesVideoGames=true;
+
+
+
+
+
+console.log(name,age,favoriteColor,SchoolsName,country,hobbies,FavoriteSong,FavoriteMovie,FunFact,likesVideoGames)
